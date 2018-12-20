c876e1f7e0563cbaa1b37a9e5c043ee395efad65
Jack Skrable <jskrable@bu.edu>
2018-12-19 20:04:18 -0500

    added headers and split modal to columns

--- middleware/handle-query.py
+++ middleware/handle-query.py
@@ -10,22 +10,27 @@ ddb = boto3.resource('dynamodb')
 response_table = ddb.Table('dev-responses')
 """
 
-test_event = {"query": "weather"}
+test_event = {"query": "whats the weather like"}
 
 def lambda_handler(event, context):
 
     interrogatives = ['who','what','when','where','why','how','which','whose']
-    #keywords = ['watch','eat','weather']
+    keywords = ['watch','eat','weather','name','game','win','live','are','you']
     query = event['query']
-    terms = query.split(' ')
+    # terms = query.split(' ')
+    score = []
+    [score.append(x) for x in interrogatives if x in query]
+    [score.append(x) for x in keywords if x in query]
+    print(score)
 
-    if 'what' and 'your' and 'name' in query:
+    if 'what' and 'name' in score:
         response = "I'm Uncle Paul. It says my name all over the screen."
-    elif 'where' and 'are' and 'you' in query:
+    elif 'where' and 'you' and 'live' in score:
         response = "I live in this computer now. I don't even have internet. Pretty cool, right?"
+    elif 'what' and 'weather' in score:
+        response = "Hey, Pop... Never mind, he's asleep."
     else:
         response = "You've got something on your shirt..."
-        
 
     return {
         'statusCode': 200,
